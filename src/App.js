0bb50130de7b83fa1d3edc6f50780a5d5a7bb8e4
Nirbhay1910 <nirbhay1910@gmail.com>
2021-05-22 12:55:12 +0530

    added new room functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import Proto from './Proto';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
-  return <div></div>;
+  const [roomId, setRoomId] = useState();
+  useEffect(() => {
+    setRoomId(uuidv4());
+  }, []);
+
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/:roomId'>
+            <Proto />
+          </Route>
+          <Route path='/'>
+            <Link to={`/${roomId}`}>CREATE ROOM</Link>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
